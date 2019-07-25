dac4f373160954bbf633d5b5cc9205f5c87e3e12
dadehen <1048769621@qq.com>
2019-07-25 10:08:15 +0800

    添加项目

---
+++ UDP服务端/SocketUPTServer/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace SocketUPTServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.No = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.IP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Port = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.HeartTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.HeartNum = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Type = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.No,
+            this.IP,
+            this.Port,
+            this.HeartTime,
+            this.HeartNum,
+            this.Type});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 30;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(571, 309);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // No
+            // 
+            this.No.HeaderText = "编号";
+            this.No.MinimumWidth = 8;
+            this.No.Name = "No";
+            // 
+            // IP
+            // 
+            this.IP.HeaderText = "IP地址";
+            this.IP.MinimumWidth = 8;
+            this.IP.Name = "IP";
+            // 
+            // Port
+            // 
+            this.Port.HeaderText = "端口号";
+            this.Port.MinimumWidth = 8;
+            this.Port.Name = "Port";
+            // 
+            // HeartTime
+            // 
+            this.HeartTime.HeaderText = "心跳时";
+            this.HeartTime.MinimumWidth = 8;
+            this.HeartTime.Name = "HeartTime";
+            // 
+            // HeartNum
+            // 
+            this.HeartNum.HeaderText = "心跳数";
+            this.HeartNum.MinimumWidth = 8;
+            this.HeartNum.Name = "HeartNum";
+            // 
+            // Type
+            // 
+            this.Type.HeaderText = "类型";
+            this.Type.MinimumWidth = 8;
+            this.Type.Name = "Type";
+            
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 309);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn No;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IP;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Port;
+        private System.Windows.Forms.DataGridViewTextBoxColumn HeartTime;
+        private System.Windows.Forms.DataGridViewTextBoxColumn HeartNum;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Type;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ UDP服务端/SocketUPTServer/Form1.cs
@@ -0,0 +1,246 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SocketUPTServer
+{
+    public partial class Form1 : Form
+    {
+        List<RowClass> TableData = new List<RowClass>();
+        List<RowClass> OldTableData = new List<RowClass>();
+        Socket server = null;
+
+        class RowClass
+        {
+            public string No;
+            public string Ip;
+            public int Port;
+
+            /// <summary>
+            /// 心跳包 是udp下端向此端发送得时间。获取 macserver webserver时仅获取最新的
+            /// </summary>
+            public DateTime HeartTime = new DateTime();
+            /// <summary>
+            /// 心跳次数，连续心跳次数
+            /// </summary>
+            public int HeartNum = 0;
+            /// <summary>
+            ///  type 分为 macserver macclient webserver webclient，有可能会在后面加 webserver_1 webclient_1
+            /// </summary>
+            public string Type; 
+        }
+
+        class ClientData
+        {
+            public string Type; 
+            public string No;
+            public string Data;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 100;
+            timer1.Tick += Timer1_Tick;
+            timer1.Start();
+
+            timer2.Interval = 5000;
+            timer2.Tick += Timer2_Tick;
+            timer2.Start();
+
+            IPEndPoint loaclEndPoint = new IPEndPoint(IPAddress.Any, 8001);
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            server.Bind(loaclEndPoint);
+
+            Task.Run(() => {
+               
+                while(true)
+                {
+                    try
+                    {
+                        byte[] receiveData = new byte[1024];
+
+                        EndPoint Remote = loaclEndPoint;
+                        server.ReceiveFrom(receiveData, 0, 1024, SocketFlags.None, ref Remote);
+                        string stringData = Encoding.UTF8.GetString(receiveData);
+                        ClientData clientData = JsonConvert.DeserializeObject<ClientData>(stringData);
+
+                        string IpPort = Remote.ToString();//获取发送端的IP及端口转为String备用
+                        string ip = IpPort.Split(':')[0];
+                        int port = int.Parse(IpPort.Split(':')[1]);
+
+                        if (clientData.Type == "Ping")
+                        {
+                            bool isHaveConnect = false;
+                            for (int i = 0; i < TableData.Count; i++)
+                            { 
+                                if(TableData[i].No == clientData.No)
+                                {
+                                    isHaveConnect = true;
+
+                                    if (TableData[i].Ip == ip &&
+                                        TableData[i].Port == port)
+                                    {
+                                        TableData[i].HeartNum++;
+                                        TableData[i].HeartTime = DateTime.Now;
+                                    }
+                                    else
+                                    {
+                                        TableData[i].Ip = ip;
+                                        TableData[i].Port = port;
+                                        TableData[i].HeartNum = 0;
+                                        TableData[i].HeartTime = DateTime.Now;
+                                    }
+                                }
+                               
+                            }
+                            if(isHaveConnect == false)
+                            {
+                                RowClass udpData = new RowClass();
+                                udpData.No = clientData.No;
+                                udpData.Ip = ip;
+                                udpData.Port = port;
+                                udpData.HeartNum = 0;
+                                udpData.HeartTime = DateTime.Now;
+                                udpData.Type = clientData.Data;
+
+                                TableData.Add(udpData);
+                            }
+
+                        }
+
+                        else if (clientData.Type == "Get") // Get 
+                        {
+                            List<RowClass> runData = new List<RowClass>();
+
+                            if(clientData.Data.Contains("server"))
+                            {
+                                runData = TableData.Where(it => it.Type == clientData.Data).OrderByDescending(it=>it.HeartTime).Take(1).ToList();
+                            }
+                            if (clientData.Data.Contains("client"))
+                            {
+                                runData = TableData.Where(it => it.Type == clientData.Data).OrderByDescending(it => it.HeartTime).ToList();
+                            }
+                            else if(clientData.Data=="All")
+                            {
+                                runData = TableData.ToList();
+                            }
+
+                            SocketUdpSend(ip, port, "GetData", JsonConvert.SerializeObject(runData));
+                        }
+
+                        else if (clientData.Type == "Send") // Get 
+                        {
+                            RowClass SendClient = new RowClass();
+
+                            SendClient = TableData.Where(it => it.No == clientData.No).FirstOrDefault();
+
+                            if(SendClient!=null)
+                            {
+                                SocketUdpSend(SendClient.Ip, SendClient.Port, "Send", IpPort);
+                            }
+                        }
+
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            });
+        }
+
+        private void SocketUdpSend(string ip, int port, string type, string data)
+        {
+            //Socket socket = new Socket(ipe.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
+            //socket.Connect(ipe);
+            if (server!=null && server.Connected)
+            {
+                ClientData sendData = new ClientData();
+                sendData.Type = type;
+                sendData.Data = data;
+
+                string sendStrData = JsonConvert.SerializeObject(sendData);
+                byte[] sendByteData = Encoding.UTF8.GetBytes(sendStrData);
+                IPEndPoint ipe = new IPEndPoint(IPAddress.Parse(ip), port);
+                server.SendTo(sendByteData, sendByteData.Length, 0, ipe);
+            }
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            bool isEqual = true;
+
+            if(TableData.Count!=OldTableData.Count)
+            {
+                isEqual = false;
+            }
+
+
+            for (int i = 0; isEqual && i < TableData.Count; i++)
+            {
+                if(JsonConvert.SerializeObject(TableData[i])!= JsonConvert.SerializeObject(OldTableData[i]))
+                {
+                    isEqual = false;
+                }
+            }
+
+            if(isEqual == false)
+            {
+                OldTableData = JsonConvert.DeserializeObject<List<RowClass>>(JsonConvert.SerializeObject(TableData));
+
+                dataGridView1.Rows.Clear();
+                foreach (var item in OldTableData)
+                {
+                    dataGridView1.Rows.Add(item.No,item.Ip,item.Port,item.HeartTime.ToString("HH:dd:ss"), item.HeartNum.ToString(),item.Type);
+                }
+            }
+           
+        }
+
+        private void Timer2_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                if(OldTableData.Count(it=>it.HeartTime.AddMilliseconds(10000)>DateTime.Now && it.Type == "webserver")==0)
+                {
+                    Method method = Method.GET;
+                    RestRequest request = new RestRequest(method);
+                    request.AddParameter("ip", "");
+                    request.AddParameter("port", 8001);
+                    RestClient client = new RestClient(); 
+                    client.BaseUrl = "http://btcjqr.cn/admin/api/mysystem/udpwebserver";
+                    IRestResponse response = client.Execute(request);
+                }
+
+                foreach (var item in OldTableData)
+                {
+                    SocketUdpSend(item.Ip, item.Port, "Ping", "Udp中转服务");
+                }
+
+                OldTableData = OldTableData.Where(it => it.HeartTime.AddHours(1) > DateTime.Now).ToList();
+            }
+            catch
+            {
+
+            }
+        } 
+    }
+}
